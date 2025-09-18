29b27e5c59a78b60c6a69567d2d92765cf5471b4
alanlopeztechio <alan.lopez@g-tech.io>
2025-09-18 16:09:31 -0600

    UPLOADING CHANGES

--- apps/informative-site/src/blocks/RenderBlocks.tsx
+++ apps/informative-site/src/blocks/RenderBlocks.tsx
@@ -7,6 +7,7 @@ import { CallToActionBlock } from '@/blocks/CallToAction/Component'
 import { ContentBlock } from '@/blocks/Content/Component'
 import { FormBlock } from '@/blocks/Form/Component'
 import { MediaBlock } from '@/blocks/MediaBlock/Component'
+import { Acordeon } from './Acordeon/Component'
 
 const blockComponents = {
   archive: ArchiveBlock,
@@ -14,7 +15,7 @@ const blockComponents = {
   cta: CallToActionBlock,
   formBlock: FormBlock,
   mediaBlock: MediaBlock,
-  acordeon: ArchiveBlock,
+  acordeon: Acordeon,
 }
 
 export const RenderBlocks: React.FC<{
